ab2d518b429c9ffc72636b8e3b5a4634e45d8c28
Azrin Hadahi <mdazrin.programmer@gmail.com>
2022-08-04 22:31:53 +0800

    Adding new file

--- test.js
+++ test.js
@@ -7,7 +7,7 @@ Create Object using literals and 'new' keyword
 let a = {x:1,y:2};
 console.log(a.x + a.y);
 
-//create object using new: same as  {}
+//create object using new; same as {}
 let b = new Object;
 b.x = 1;
 console.log(b.x);
