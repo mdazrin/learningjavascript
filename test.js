27695a50a924bdf0a5f41cbe9dd7c6e10b0488e2
Azrin Hadahi <mdazrin.programmer@gmail.com>
2022-08-04 21:21:18 +0800

    Adding new file

---
+++ test.js
@@ -0,0 +1,13 @@
+/*Lesson: Object
+Create Object using literals and 'new' keyword
+*/
+
+//create object using literal
+//literal meaning {name:value}
+let a = {x:1,y:2};
+console.log(a.x + a.y);
+
+//create object using new: same as  {}
+let b = new Object;
+b.x = 1;
+console.log(b.x);
